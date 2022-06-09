b8bd3ba31d65038439714ae1e82530d41aa1351d
shtabdeepaull <shtabdee30@gmail.com>
2022-06-09 22:17:11 +0600

    added header and banner

---
+++ src/pages/Banner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import banner from '../images/banner.png'
+
+
+const Banner = () => {
+    return (
+        <div className=''>
+            <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row">
+                <div class="avatar">
+                    <div class="w-50 mask mask-squircle">
+                        <img src={banner} />
+                    </div>
+                </div>
+                <div>
+                    <h1 class="text-5xl font-bold">Hi! I am Shtabdee</h1>
+                    <p class="py-6 text-white">A passionate Full Stack Web Developer having an experience of building web applications with Javascript/Reactjs/Nodejs and some other cool libraries and frameworks.</p>
+                     <a href="https://drive.google.com/file/d/1ciGBtS9YX5rFx3b7aDJoAUL0vxWvMMcQ/view?usp=sharing"><button className='btn w-24 text-white'>Resume</button></a> 
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/pages/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    const menuItems = <>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="https://drive.google.com/file/d/1ciGBtS9YX5rFx3b7aDJoAUL0vxWvMMcQ/view?usp=sharing">My Resume</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Skill</a></li>
+    </>
+    return (
+        <div class="navbar  bg-base-200">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label tabindex="0" class="btn  btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <h2 class="text-2xl">Shtabdee Paul</h2>
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+                  {menuItems}
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
