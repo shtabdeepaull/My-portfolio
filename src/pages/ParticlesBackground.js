7b7ae2584309ebb232d6810b2faffbfd7c08cb1c
shtabdeepaull <shtabdee30@gmail.com>
2022-06-11 23:35:40 +0600

    added responsive skills section

--- src/pages/ParticlesBackground.js
+++ src/pages/ParticlesBackground.js
@@ -55,3 +55,35 @@ const ParticlesBackground = () => {
 }
 
 export default ParticlesBackground;
+
+{/* <h2 className=' text-center text-white text-xl font-bold'>My Skills</h2>
+
+<div className=' flex justify-center sm-flex-row mb-8'>
+    <figure className=" px-10 pt-10">
+        <img src={html} alt="Shoes" class="rounded-xl" />
+    </figure>
+
+    <figure class="px-10 pt-10">
+        <img src={css} alt="Shoes" class="rounded-xl" />
+    </figure>
+
+    <figure class="px-10 pt-10">
+        <img src={javascript} alt="Shoes" class="rounded-xl" />
+    </figure>
+
+    <figure class="px-10 pt-10">
+        <img src={node} alt="Shoes" class="rounded-xl" />
+    </figure>
+
+    <figure class="px-10 pt-10">
+        <img src={netlify} alt="Shoes" class="rounded-xl" />
+    </figure>
+
+    <figure class="px-10 pt-10">
+        <img src={react} alt="Shoes" class="rounded-xl" />
+    </figure>
+
+    <figure class="px-10 pt-10">
+        <img src={git} alt="Shoes" class="rounded-xl" />
+    </figure>
+</div> */}
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -10,9 +10,19 @@ import git from '../images/Git.png';
 const Skills = () => {
     return (
         <div>
-            <h2 className=' text-center text-white text-xl font-bold'>My Skills</h2>
+            <h2 className=' text-center text-white text-5xl font-bold'>My Skills</h2>
 
-            <div className=' flex justify-center mb-8'>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 m-8'>
+                <img className='px-10 pt-10 rounded xl' src={html} alt="pic" />
+                <img className='px-10 pt-10' src={css} alt="pic" />
+                <img className='px-10 pt-10' src={react} alt="pic" />
+                <img className='px-10 pt-10' src={node} alt="pic" />
+                <img className='px-10 pt-10' src={javascript} alt="pic" />
+                <img className='px-10 pt-10' src={netlify} alt="pic" />
+                <img className='px-10 pt-10' src={git} alt="pic" />
+            </div>
+
+            {/* <div className=' flex justify-center  sm:flex-row mb-8'>
                 <figure className=" px-10 pt-10">
                     <img src={html} alt="Shoes" class="rounded-xl" />
                 </figure>
@@ -40,7 +50,7 @@ const Skills = () => {
                 <figure class="px-10 pt-10">
                     <img src={git} alt="Shoes" class="rounded-xl" />
                 </figure>
-            </div>
+            </div> */}
 
         </div>
 
