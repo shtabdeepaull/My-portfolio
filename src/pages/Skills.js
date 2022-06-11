9c21458daa3430f9c27e3e2ccd0bfc9fc4754016
shtabdeepaull <shtabdee30@gmail.com>
2022-06-12 00:05:16 +0600

    added final touch in skill section

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -10,16 +10,16 @@ import git from '../images/Git.png';
 const Skills = () => {
     return (
         <div>
-            <h2 className=' text-center text-white text-5xl font-bold'>My Skills</h2>
+            <h2 className=' text-center text-white text-5xl font-bold mb-0'>My Skills</h2>
 
-            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 m-8'>
-                <img className='px-10 pt-10 rounded xl' src={html} alt="pic" />
-                <img className='px-10 pt-10' src={css} alt="pic" />
-                <img className='px-10 pt-10' src={react} alt="pic" />
-                <img className='px-10 pt-10' src={node} alt="pic" />
-                <img className='px-10 pt-10' src={javascript} alt="pic" />
-                <img className='px-10 pt-10' src={netlify} alt="pic" />
-                <img className='px-10 pt-10' src={git} alt="pic" />
+            <div className='container grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 m-20 '>
+                <img className='px-10 m-2 pt-10 w-56 shadow-lg shadow-white ' src={html} alt="pic" />
+             <img className='px-10 m-2 pt-10 w-56 shadow-lg shadow-white' src={css} alt="pic" />
+                <img className='px-10 m-2 pt-10 w-56 shadow-lg shadow-white' src={react} alt="pic" />
+                <img className='px-10 m-2 pt-10 w-56 shadow-lg shadow-white' src={node} alt="pic" />
+                <img className='px-10 m-2 pt-10 w-56 shadow-lg shadow-white' src={netlify} alt="pic" />
+                <img className='px-10 m-2 pt-10 w-56 shadow-lg shadow-white' src={git} alt="pic" />
+                <img className='px-10 m-2 pt-10 w-56 shadow-lg shadow-white' src={javascript} alt="pic" />
             </div>
 
             {/* <div className=' flex justify-center  sm:flex-row mb-8'>
